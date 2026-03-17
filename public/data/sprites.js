862f32e05265fcd5b1a46c50631d83b0932aabcd
Tewahh <sabellad0587@bavts.org>
2026-03-17 12:55:52 +0000

    Fixed everything from my last screw up, backing this up for when i switch fully to using a spritesheet.

---
+++ public/data/sprites.js
@@ -0,0 +1,10 @@
+export const SPRITES = {
+  player: {
+    idle: { x: 0, y: 0, w: 64, h: 64 },
+    attack: { x: 64, y: 0, w: 64, h: 64 },
+  },
+
+  sword: {
+    icon: { x: 0, y: 128, w: 32, h: 32 },
+  }
+};
